90a14f10607d5c42ba905eb05d1e358193e2639f
Peter Yu <umd706881@gmail.com>
2017-10-02 13:58:05 -0400

    test

---
+++ smartapps/testetesting/testing123321.src/testing123321.groovy
@@ -0,0 +1,41 @@
+/**
+ *  testing123321
+ *
+ *  Copyright 2017 Peter
+ *
+ */
+definition(
+    name: "testing123321",
+    namespace: "testetesting",
+    author: "Peter",
+    description: "test",
+    category: "",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	section("Title") {
+		// TODO: put inputs here
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
